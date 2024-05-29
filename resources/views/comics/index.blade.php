3f5f2e0ed7c8de15abed7624b39e00d47f33ea9e
Marcomagnano93 <marcog.magnano@gmail.com>
2024-05-29 15:06:24 +0200

    ADD: comics index (comics folder)

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Index')
+
+@section('content')
+    <div class="container">
+        <h1>Questo è l'index</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// CRUD [Create Read Update Delete]
+
+Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
